dcfe815e3fb4a3751d2a8d1134df3cabc75ee01b
Rudranil8 <95670098+Rudranil8@users.noreply.github.com>
2024-04-16 22:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// function stringChop(str, size) {
+// 	if(!str){
+// 		return [];
+// 	}
+// 	let result=[];
+// 	for(let i=0; i<str.length; i=i+size){
+// 		result.push(str.substring(i, i+size));
+// 	}
+// }
+// const str = prompt("Enter String.");
+// const size = prompt("Enter Chunk Size.");
+// alert(stringChop(str, size));
 function stringChop(str, size) {
 	if(!str){
 		return [];
@@ -6,6 +18,7 @@ function stringChop(str, size) {
 	for(let i=0; i<str.length; i=i+size){
 		result.push(str.substring(i, i+size));
 	}
+	return result;
 }
 
 const str = prompt("Enter String.");
